d40d2de77f519612f52a23957c40d579083367da
visualstudio <visualstudio@122-PC03>
2019-03-29 20:11:05 -0400

    commit 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
-namespace ConsoleApp1
+namespace C0727250
+
+// Name :- Kunal Sharma//
+  //student id :- c0727250//
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("downloading a file");
+            Download();
+            Console.ReadLine();
         }
+
+
+        static void Download()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Download Complete");
+        }
+
     }
 }
